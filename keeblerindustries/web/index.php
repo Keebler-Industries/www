19461ae6109e7b9b112308883fc5a43dd97ec98a
Keebler17 <keebler17.exe@gmail.com>
2022-09-05 09:37:48 -0400

    init

---
+++ keeblerindustries/web/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Keebler Industries</title>
+        <?php readfile("/var/server/www/keeblerindustries/web/resources/header.html"); ?>
+    </head>
+    <body>
+        <?php readfile("/var/server/www/keeblerindustries/web/resources/navbar.html"); ?>
+        <div id="content"></div>
+        <div id="footer"></div>
+    </body>
+</html>
---
+++ keeblerindustries/web/salmon.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>salmon</title>
+        <?php readfile("/var/server/www/keeblerindustries/web/resources/header.html"); ?>
+    </head>
+    <body>
+        <?php readfile("/var/server/www/keeblerindustries/web/resources/navbar.html"); ?>
+        <img style="background-repeat: repeat;" src="resources/images/salmon.webp">
+    </body>
+</html>
