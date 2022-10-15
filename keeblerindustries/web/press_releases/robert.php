a60e1ecc7b69cbbc756abbfbd4960b0438eda334
Keebler17 <cheddarperson493@gmail.com>
2022-10-15 02:20:25 -0400

    added content for robert press release

--- keeblerindustries/web/press_releases/robert.php
+++ keeblerindustries/web/press_releases/robert.php
@@ -9,6 +9,21 @@
         <?php readfile("/var/server/www/keeblerindustries/web/resources/navbar.html"); ?>
         <div id="content">
             <h1>Keebler Industries Int'l Assoc. has determined that Robert Liam Fahie has breached our Core Principles and we will be hereby denouncing all of his further actions.</h1>
+            <hr>
+            <p>Robert Liam Fahie, for far too long, has continously offended many employees of this company, including, but not limited to: Luciano Xavier Pomara, James Frederick
+                Sears, and Keebler James 17. Robert Liam Fahie is the antithesis of our company, and we here at Keebler Industries firmly believe we should mass report his Twitter
+                account. We have some specific grievences:<br>
+                <ol>
+                    <li>His Discord status SUCKS</li>
+                    <li>His Terraria world SUCKS</li>
+                    <li>He SUCKS at python</li>
+                </ol>
+                Due to these reasons, we have decided to terminate the employee. You may email him hate mail at: <a href="mailto:robbie@keeblerindustries.com">robbie@keeblerindustries.com</a>
+                <br><br>
+                Thank you,
+                <br>
+                Keebler Industries Team
+            </p>
         </div>
         <div id="footer"></div>
     </body>
