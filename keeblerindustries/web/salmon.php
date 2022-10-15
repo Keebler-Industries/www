da90ded7f81d0d7c7361fe6e7b57ffb925d39aee
Keebler17 <cheddarperson493@gmail.com>
2022-10-15 14:13:57 -0400

    removal of the salmon page

--- keeblerindustries/web/salmon.php
+++ keeblerindustries/web/salmon.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html>
     <head>
         <title>salmon</title>
@@ -8,4 +8,4 @@
         <?php readfile("/var/server/www/keeblerindustries/web/resources/navbar.html"); ?>
         <img style="background-repeat: repeat;" src="resources/images/salmon.webp">
     </body>
-</html>
+</html> -->
