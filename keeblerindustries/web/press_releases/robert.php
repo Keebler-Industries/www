238cff0f5df593b45f7ca4598a62c34138d33c99
Keebler17 <cheddarperson493@gmail.com>
2022-10-19 21:27:35 -0400

    g

--- keeblerindustries/web/press_releases/robert.php
+++ keeblerindustries/web/press_releases/robert.php
@@ -1,17 +1,17 @@
-<!-- <!DOCTYPE html>
+<!DOCTYPE html>
 <html>
     <head>
-        <title>Denouncing Robert Fahie</title>
+        <!-- <title>Denouncing Robert Fahie</title> -->
         <link rel="stylesheet" href="resources/press.css">
         <?php readfile("/var/server/www/keeblerindustries/web/resources/header.html"); ?>
         <meta property="og:title" content="Keebler Industries">
-        <meta property="og:description" content="Press Releases &mdash; Denouncing of Robert Liam Fahie">
+        <!-- <meta property="og:description" content="Press Releases &mdash; Denouncing of Robert Liam Fahie"> -->
         <meta property="og:image" content="https://www.keeblerindustries.com/resources/icon.png">
     </head>
     <body>
         <?php readfile("/var/server/www/keeblerindustries/web/resources/navbar.html"); ?>
         <div id="content">
-            <h1>Keebler Industries Int'l Assoc. has determined that Robert Liam Fahie has breached our Core Principles and we will be hereby denouncing all of his further actions.</h1>
+            <!-- <h1>Keebler Industries Int'l Assoc. has determined that Robert Liam Fahie has breached our Core Principles and we will be hereby denouncing all of his further actions.</h1>
             <hr>
             <p>Robert Liam Fahie, for far too long, has continously offended many employees of this company, including, but not limited to: Luciano Xavier Pomara, James Frederick
                 Sears, and Keebler James 17. Robert Liam Fahie is the antithesis of our company, and we here at Keebler Industries firmly believe we should mass report his Twitter
@@ -26,8 +26,8 @@
                 Thank you,
                 <br>
                 Keebler Industries Team
-            </p>
+            </p> -->
         </div>
         <div id="footer"></div>
     </body>
-</html> -->
+</html>
